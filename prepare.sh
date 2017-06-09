caf79e206965e706d11dff8942b3a8a5f538a664
eiva <me@eiva.info>
2017-06-09 06:00:41 +0300

    Added rpi_ws281x lib to prepare

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
 
-apt install build-essentials python3 python3-pip rpi.gpio python-smbus i2c-tools requests scons
+apt install build-essentials python3 python3-pip rpi.gpio python-smbus i2c-tools requests scons swig
+
+git clone https://github.com/jgarff/rpi_ws281x.git
+cd rpi_ws281x
+scons
+
+cd python
+python3 setup.py install
 
